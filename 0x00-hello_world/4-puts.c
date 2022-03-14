14e88837e343b734992a6571b6a37eb9da3b6521
danielfemi <somuyiwadaniel@yahoo.com>
2022-03-14 01:17:03 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
+/*
+ *  * File: 4-puts.c
+ *   * Auth: Femi Daniel Somuyiwa somuyiwadaniel@yahoo.com>
+ *    */
+
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void){
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
+{
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
-              }
+}
